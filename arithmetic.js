3faef45689eeaf1548b85bbf342b7bdc4d7c2ba8
Pallab Barman <76774117+pallabbarman@users.noreply.github.com>
2022-05-22 10:45:14 +0600

    arithmetic

---
+++ arithmetic.js
@@ -0,0 +1,44 @@
+let a = 10;
+let b = 20;
+
+// addition
+const c = a + b;
+console.log(c);
+
+// subtraction
+const d = a - b;
+console.log(d);
+
+// multiplication
+const e = a * b;
+console.log(e);
+
+// division
+const f = a / b;
+console.log(f);
+
+// remainder
+const g = a % b;
+console.log(g);
+
+// increment
+a += 1;
+const h = a;
+console.log(h);
+
+// decrement
+b -= 1;
+const i = b;
+console.log(i);
+
+// exponentiation
+const j = a ** b;
+console.log(j);
+
+// ** produces the same result as Math.pow()
+const k = a ** b;
+console.log(k);
+
+// precedence
+const l = 100 + 50 * 3;
+console.log(l);
---
+++ syntax.js
@@ -0,0 +1,15 @@
+// Fixed values are called Literals
+
+const x = 10;
+const y = 'Pallab';
+console.log(x);
+console.log(y);
+
+// Variable values are called Variables
+
+let p;
+p = 20;
+let q;
+q = 'Barman';
+console.log(p);
+console.log(q);
